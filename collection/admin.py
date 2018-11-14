85871f8e403f4923adbca58e5c0e63836abb7406
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-11-14 13:34:54 -0500

    inital commit before things get too crazy

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from collection.models import Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    model = Blog
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+    
+admin.site.register(Blog, BlogAdmin)
+
 # Register your models here.
--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    number = 6
+    number = 2
     thing = "Thing name"
     return render(request, 'index.html', {'number': number, 'thing': thing,})
