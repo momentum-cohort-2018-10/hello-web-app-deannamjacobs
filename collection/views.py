4deac23adb0818ac94bf78a08269bc128a2e9a6d
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-11-15 14:39:29 -0500

    initial commit today

--- collection/views.py
+++ collection/views.py
@@ -3,5 +3,9 @@ from collection.models import Blog
 
 def index(request):
     blogs = Blog.objects.all()
-    thing = "Thing name"
+    blog = "Blog name"
     return render(request, 'index.html', {'blogs': blogs,})
+
+def blog_detail(request, slug):
+    blog = Blog.objects.get(slug=slug)
+    return render(request, 'blogs/blog_detail.html', {'blog': blog,})
